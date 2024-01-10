3f8f936ad390f55bf0660c4834cc005d87e8783f
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-09 23:43:00 -0500

    Update Currency Converter.java
    Added enum 'CurrencyCode' for currency code, Changed 'Currency' class to 'CurrencyCode' enum, Changed switch statements to enum values.

--- Currency Converter.java
+++ Currency Converter.java
@@ -5,40 +5,44 @@ import java.util.HashMap;
 
 public class Currency {
 
-    private String name;
-    private String shortName;
-    private HashMap<String, Double> exchangeRates = new HashMap<>();
+    public enum CurrencyCode {
+        USD, EUR, GBP, CHF, CNY, JPY
+    }
 
-    public Currency(String name, String shortName) {
+    private String name;
+    private CurrencyCode currencyCode;
+    private HashMap<CurrencyCode, Double> exchangeRates = new HashMap<>();
+
+    public Currency(String name, CurrencyCode currencyCode) {
         this.name = name;
-        this.shortName = shortName;
+        this.currencyCode = currencyCode;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getShortName() {
-        return shortName;
+    public CurrencyCode getCurrencyCode() {
+        return currencyCode;
     }
 
-    public HashMap<String, Double> getExchangeRates() {
+    public HashMap<CurrencyCode, Double> getExchangeRates() {
         return exchangeRates;
     }
 
-    public void setExchangeRate(String currencyCode, Double rate) {
-        this.exchangeRates.put(currencyCode, rate);
+    public void setExchangeRate(CurrencyCode code, Double rate) {
+        this.exchangeRates.put(code, rate);
     }
 
     public void setDefaultExchangeRates() {
         switch (name) {
             case "US Dollar":
-                setExchangeRate("USD", 1.00);
-                setExchangeRate("EUR", 0.93);
-                setExchangeRate("GBP", 0.66);
-                setExchangeRate("CHF", 1.01);
-                setExchangeRate("CNY", 6.36);
-                setExchangeRate("JPY", 123.54);
+                setExchangeRate(CurrencyCode.USD, 1.00);
+                setExchangeRate(CurrencyCode.EUR, 0.93);
+                setExchangeRate(CurrencyCode.GBP, 0.66);
+                setExchangeRate(CurrencyCode.CHF, 1.01);
+                setExchangeRate(CurrencyCode.CNY, 6.36);
+                setExchangeRate(CurrencyCode.JPY, 123.54);
                 break;
             // Repeat for other currencies
         }
