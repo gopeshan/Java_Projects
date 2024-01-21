0a526825549036cc42fd745a4b4bff6b0e3beea7
Gopeshan Mathanachandran <122761640+gopeshan@users.noreply.github.com>
2024-01-21 17:56:53 -0500

    Update Task Manager.java
    Added Task and TaskManager class. Both are not finished.

--- Task Manager.java
+++ Task Manager.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+}
+
+class TaskManager {
+    private ArrayList<Task> tasks;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        Task newTask = new Task(description);
+        tasks.add(newTask);
+        System.out.println("New task added: \"" + description + "\"");
+    }
+
+    
+}
